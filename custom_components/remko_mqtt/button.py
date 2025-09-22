f32555e7fc5e6dfc04d1c8a37c57a385d4457a85
Sven Klomp <mail@klomp.eu>
2025-09-22 11:09:56 +0200

    Solve error

--- custom_components/remko_mqtt/button.py
+++ custom_components/remko_mqtt/button.py
@@ -28,10 +28,7 @@ from .remko_regs import (
     reg_id,
 )
 
-if TYPE_CHECKING:
-    from functools import cached_property
-else:
-    from homeassistant.backports.functools import cached_property
+from functools import cached_property
 
 _LOGGER = logging.getLogger(__name__)
 
